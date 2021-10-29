cefbfabf4e55c8ef9e08e0d90dd4d459b726521d
Prometheus <atommielcarek@gmail.com>
2021-10-29 12:32:09 -0600

    Updates

---
+++ precache-manifest.71249c3239de0fd59b0fe8cf86fe42dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1264dfa1feb7d0755cb717d43dfa77a",
+    "url": "/codex-leicester/index.html"
+  },
+  {
+    "revision": "890ef720e77611cc93de",
+    "url": "/codex-leicester/static/css/2.967acb48.chunk.css"
+  },
+  {
+    "revision": "26af3e561daa53d2b143",
+    "url": "/codex-leicester/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "890ef720e77611cc93de",
+    "url": "/codex-leicester/static/js/2.a23d3ec4.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/codex-leicester/static/js/2.a23d3ec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26af3e561daa53d2b143",
+    "url": "/codex-leicester/static/js/main.d9e0ea9c.chunk.js"
+  },
+  {
+    "revision": "ae628d7d337e330a6bd6",
+    "url": "/codex-leicester/static/js/runtime-main.5e5b7a1b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cowboicoding.com/precache-manifest.bdc1b60092dcfd9283596185d7802e7b.js"
+  "/codex-leicester/precache-manifest.71249c3239de0fd59b0fe8cf86fe42dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cowboicoding.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codex-leicester/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
